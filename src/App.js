eea63e7bbff0b193b33a29f58bccbc07ae372787
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-09-22 23:27:31 +0300

    connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      cityQuery:'',
+      weatherDataArr:{},
+      showData: false
+    }
+  }
+
+  getCityInfo = async (e) =>{
+    e.preventDefault()
+    //http://localhost:3001/weather?city=amman
+
+  await this.setState({
+      cityQuery: e.target.cityQuery.value,
+      // weatherDataArr: weatherData.data
+    })
+
+    let url = `https://weather-class-08.herokuapp.com/weather?city=${this.state.cityQuery}`
+    console.log(url)
+
+    let weatherData = await axios.get(url)
+
+    this.setState({
+      weatherDataArr: weatherData.data,
+      showData: true
+    })
+    console.log(this.state.weatherDataArr)
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Lab 08 </h1>
+        <h3>Type the city you would like to get information about</h3>
+        <form onSubmit={this.getCityInfo}>
+          <input type="text" placeholder="Enter the city name" name='cityQuery'/>
+          <button >Get info</button>
+          <p></p>
+        </form>
+        {
+          this.state.showData && 
+          <>
+          {this.state.weatherDataArr.map(element => {
+            return (
+              <>
+              <p>{element.description}</p>
+              <p>{element.date}</p>
+              </>
+            )
+          })}
+          </>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
